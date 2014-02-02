f71f692c8b240d657ab26b0224a5f1230a60e8ad
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-02-02 13:34:29 -0800

    save data to a spreadsheet

--- learn-rails/app/models/contact.rb
+++ learn-rails/app/models/contact.rb
@@ -12,4 +12,18 @@ class Contact < ActiveRecord::Base
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
 
+  def update_spreadsheet
+    connection = GoogleDrive.login(ENV["GMAIL_USERNAME"], ENV["GMAIL_PASSWORD"])
+    ss = connection.spreadsheet_by_title('Learn-Rails-Example')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Learn-Rails-Example')
+    end
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    ws[last_row, 1] = Time.new
+    ws[last_row, 2] = self.name
+    ws[last_row, 3] = self.email
+    ws[last_row, 4] = self.content
+    ws.save
+  end
 end
